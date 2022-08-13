d1c7bb48c8792e54d046f871550822b1113e69e9
Mykola Opalenko <mykola.opalenko@gmail.com>
2022-08-13 14:10:11 +0300

    done 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
 
+const stopBtn = document.querySelector("[data-stop]");
+const startBtn = document.querySelector("[data-start]");
+const body = document.querySelector("body");
+let backGround = null;
+
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStartBtn() {
+   startBtn.disabled = true;
+   stopBtn.disabled = false;
+
+   backGround = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor()
+   }, 1000);
+};
+
+function onStopBtn() {
+   clearInterval(backGround);
+
+   stopBtn.disabled = true;
+   startBtn.disabled = false;
+};
+
+
+startBtn.addEventListener('click', onStartBtn);
+
+stopBtn.addEventListener('click', onStopBtn);
+
+// document.addEventListener('keydown', (event) => {
+//    if (event.code === "Space") {
+//       onStartBtn();
+//    };
+// })
